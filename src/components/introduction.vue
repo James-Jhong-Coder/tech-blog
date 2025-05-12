d40aa27d5d9f7f8f0f953eaf842c210c2242df79
james jhong <jamesjhong@star-link.tech>
2025-05-12 23:11:13 +0800

    加上 header footer body

---
+++ src/components/introduction.vue
@@ -0,0 +1,12 @@
+<script></script>
+
+<template>
+    <div class="flex flex-col text-5xl">
+        <span>Hello, 我是 James我是一名前端工程師</span>
+        <span class="mt-3">喜歡研究前端技術 </span>
+    </div>
+</template>
+
+<style>
+
+</style>
