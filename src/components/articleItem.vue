f3e36008997154ccd32289f42ab55da00cfe02b5
james jhong <jamesjhong@star-link.tech>
2025-05-15 01:40:41 +0800

    add article item

---
+++ src/components/articleItem.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+    post: {
+        type: Object,
+        default: () => {
+            return {};
+        }
+    }
+})
+
+</script>
+
+<template>
+    <div class="box">
+        <div class="box__title">
+            <a :href="`/posts/${post.id}`">{{ post.data.title }}</a>
+        </div>
+        <div class="box-description">{{ post.data.description }}</div>
+    </div>
+</template>
+
+<style scoped>
+@reference "../styles/global.css";
+
+.box {
+    @apply flex flex-col border border-gray-500;
+    @apply w-full;
+    @apply py-4 px-6 rounded-lg;
+}
+
+.box .box__title {
+    @apply text-base font-bold;
+}
+
+.box-description {
+    @apply text-base font-bold;
+}
+</style>
