3aced0ae6377745d31ba71c71c3f87e606b6841e
james jhong <jamesjhong@star-link.tech>
2025-05-15 01:41:25 +0800

    加上部落格 content

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+import { glob, file } from 'astro/loaders';
+
+const blog = defineCollection({
+    loader: glob({
+        pattern: '**/*.md',
+        base: './src/content/blog',
+    }),
+    schema: z.object({
+        title: z.string(),          // 必填
+        description: z.string(),          // 必填
+        pubDate: z.coerce.date(),     // 必填，會自動轉成 Date
+        updatedDate: z.coerce.date().optional(), // 選填
+    }),
+})
+
+export const collections = { blog };
