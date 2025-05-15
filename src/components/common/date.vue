8246077d5a1cbf685fb891f8029c2b7479ef93f0
james jhong <jamesjhong@star-link.tech>
2025-05-15 08:04:09 +0800

    add article component

---
+++ src/components/common/date.vue
@@ -0,0 +1,13 @@
+<script setup>
+import dateIcon from '@icons/date.svg?url';
+</script>
+
+<template>
+    <div class="flex items-center">
+        <img :src="dateIcon" alt="date" />
+        <span class="text-xs ml-1">
+            <slot />
+        </span>
+    </div>
+</template>
+
---
+++ src/components/common/tag.vue
@@ -0,0 +1,21 @@
+<script setup>
+import tagIcon from '@icons/tag.svg?url';
+</script>
+
+<template>
+    <div class="tag">
+        <img :src="tagIcon" alt="tagIcon" />
+        <span class="text-xs ml-1">
+            <slot />
+        </span>
+    </div>
+</template>
+
+
+<style scoped>
+@reference "../../styles/global.css";
+.tag {
+    @apply inline-flex items-center;
+    @apply bg-gray-500 rounded-lg p-2 text-white;
+}
+</style>
