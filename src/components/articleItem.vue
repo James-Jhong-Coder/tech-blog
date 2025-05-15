8d540e8465cb1bd0d5ff7fff85d3c85304cfa049
james jhong <jamesjhong@star-link.tech>
2025-05-15 08:07:02 +0800

    article item 加上日期和標籤

--- src/components/articleItem.vue
+++ src/components/articleItem.vue
@@ -1,21 +1,38 @@
 <script setup>
-defineProps({
+import Date from '@components/common/date.vue';
+import Tag from '@components/common/tag.vue';
+import dayjs from 'dayjs';
+import { computed } from 'vue';
+
+const props = defineProps({
     post: {
         type: Object,
         default: () => {
             return {};
         }
     }
+});
+
+const computedDate = computed(() => {
+    return dayjs(props.post.data.pubDate).format('YYYY-MM-DD');
 })
 
 </script>
 
 <template>
     <div class="box">
-        <div class="box__title">
+        <div class="bo-title">
             <a :href="`/posts/${post.id}`">{{ post.data.title }}</a>
         </div>
-        <div class="box-description">{{ post.data.description }}</div>
+        <div class="box-description mt-2.5">{{ post.data.description }}</div>
+        <div class="flex items-center mt-2.5">
+            <Date>{{ computedDate }}</Date>
+        </div>
+        <div class="flex items-center mt-2">
+            <Tag v-for="(item, index) in post.data.tags" :key="index" :class="{ 'ml-2': index > 0 }">
+                {{ item }}
+            </Tag>
+        </div>
     </div>
 </template>
 
@@ -25,14 +42,14 @@ defineProps({
 .box {
     @apply flex flex-col border border-gray-500;
     @apply w-full;
-    @apply py-4 px-6 rounded-lg;
+    @apply py-4 px-6 rounded-lg leading-none;
 }
 
-.box .box__title {
+.box-title {
     @apply text-base font-bold;
 }
 
 .box-description {
-    @apply text-base font-bold;
+    @apply text-xs font-bold;
 }
 </style>
