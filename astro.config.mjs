d3e650f3afeee3016a850eb43dd5e65b93343552
JamesJhong <jamesjhong@star-link.tech>
2025-09-09 06:47:02 +0800

    feat: 🎸 add vue config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+
+import vue from '@astrojs/vue';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   devToolbar: {
     enabled: false, // 關閉底部工具列
   },
+
+  integrations: [vue()],
 });
