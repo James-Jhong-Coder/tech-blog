b63321114e7e7161ed908181c1508fac5ea77a4e
james jhong <jamesjhong@star-link.tech>
2025-09-12 06:32:10 +0800

    feat: 🎸 調整 navbutton style

--- src/components/NavButton.vue
+++ src/components/NavButton.vue
@@ -6,15 +6,20 @@ const props = withDefaults(
     | string
     | Record<string, boolean>
     | Array<string | Record<string, boolean>>;
+    href?: string;
   }>(),
   {
     isActive: false,
+    href: '/'
   }
 );
+const onButtonClickHandler = () => {
+  window.location.href = props.href
+}
 </script>
 
 <template>
-  <div class="nav-btn" :class="[props.class, { 'is-active': props.isActive }]">
+  <div class="nav-btn" :class="[props.class, { 'is-active': props.isActive }]" @click="onButtonClickHandler()">
     <slot />
   </div>
 </template>
@@ -23,8 +28,8 @@ const props = withDefaults(
 @reference "../styles/global.css";
 
 .nav-btn {
-  @apply inline-flex items-center cursor-pointer leading-none;
-  @apply hover:bg-gray-150 py-1 px-2 rounded-md;
+  @apply inline-flex items-center cursor-pointer leading-none text-sm;
+  @apply hover:bg-gray-150 py-2 px-2.5 rounded-md;
 }
 
 .is-active {
